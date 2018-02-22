120a6944de22f2aac91ea3e20997d63624363997
Jacob Frøkjær Wortmann <jacob@wortmann.dk>
2018-02-22 20:44:55 +0100

    fixed identation and added depedencies

--- src/main/vuejs/src/components/electro/Electro.vue
+++ src/main/vuejs/src/components/electro/Electro.vue
@@ -1,17 +1,49 @@
 <template>
     <div class="small">
-        <line-chart :chart-data="datacollection"></line-chart>
+        <line-chart :chart-data="dataCollection"></line-chart>
     </div>
 </template>
 
 <script>
-    import {Line} from 'vue-chartjs'
+    import LineChart from './LineChart2';
+    import axios from 'axios';
 
     export default {
-        extends: Line,
-        props: ['data', 'options'],
+        components: {
+            LineChart
+        },
+        props: {},
+        data() {
+            return {
+                dataCollection: null
+            };
+        },
         mounted() {
-            this.renderChart(this.data, this.options)
+            this.requestData(200)
+        },
+        methods: {
+            requestData(numberOfItems) {
+                const meterId = 99806;
+
+                axios.get(`https://jacobwortmann.dk:9443/electro-backend/api/meter/${meterId}/groupedReadings?limit=${numberOfItems}&group=60`)
+                    .then(response => {
+                        console.log(response);
+                        this.dataCollection = {
+                            labels: response.data.map(timeSpan => timeSpan.readingTime),
+                            datasets: [{
+                                label: 'kwh per minute',
+                                backgroundColor: '#f87979',
+                                data: response.data.map(timeSpan => timeSpan.kwhSum)
+                            }]
+                        };
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            }
         }
     }
 </script>
+
+<style>
+</style>
--- src/main/vuejs/src/router/index.js
+++ src/main/vuejs/src/router/index.js
@@ -2,14 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
-import Electro from '@/components/Electro'
+import Electro from '@/components/electro/Electro'
 import Projects from '@/components/Projects'
 
 Vue.use(Router);
 
 export default new Router({
     mode: 'history',
-    base: "electro-webapp",
     routes: [
         {
             path: '/',
